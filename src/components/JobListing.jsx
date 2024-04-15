8f6ec62eb0bb3ba1b83ec4ff1f8aa912c5e9ccdd
Prince Kanu <kanuprince93@yahoo.com>
2024-04-15 12:46:38 -0500

    create state that shortens description by default

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import {useState} from 'react'
 
 function JobListing({job}) {
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+  let description = job.description
+
+  if(!showFullDescription) {
+    description = `${description.substring(0, 100)}...`
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
         <div className="p-4">
@@ -9,7 +18,7 @@ function JobListing({job}) {
             <h3 className="text-xl font-bold">{job.title}</h3>
           </div>
           <div className="mb-5">
-           {job.description}
+           {description}
           </div>
           <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
           <div className="border border-gray-100 mb-5"></div>
