ec80fdfff2b2fd0dea80d705817c3de4a438bdaa
Prince Kanu <kanuprince93@yahoo.com>
2024-04-16 15:19:46 -0500

    created a pages folder and create a homepage component to show information on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import {
   Route, createBrowserRouter, createRoutesFromElements, RouterProvider
 } from 'react-router-dom'
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import Homepage from './pages/Homepage'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>My Apps</h1>} />)
+  createRoutesFromElements(<Route index element={<h1><Homepage /></h1>} />)
 )
 
 function App() {
