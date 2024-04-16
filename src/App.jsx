f8e7e40371f7f2e6eb453cce5cbdd3e06d9c18aa
Prince Kanu <kanuprince93@yahoo.com>
2024-04-16 15:29:18 -0500

    created a layouts folder for navbar to show on every page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import {
   Route, createBrowserRouter, createRoutesFromElements, RouterProvider
 } from 'react-router-dom'
 import React from 'react'
+import MainLayout from './layouts/MainLayout'
 import Homepage from './pages/Homepage'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1><Homepage /></h1>} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<h1><Homepage /></h1>} />
+    </Route>
+    
+  )
 )
 
 function App() {
