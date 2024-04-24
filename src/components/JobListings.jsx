e8e9799eb331d8f1188969c5e214427d7501ac52
Prince Kanu <kanuprince93@yahoo.com>
2024-04-24 12:17:28 -0500

    change job listing to fetch jobs from json server and added loading prompt for page load

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
 import React from 'react'
-import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
 function JobListings({ isHome = false}) {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect( () => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/jobs")
+        const data = await res.json()
+        setJobs(data)
+      } catch (err) {
+        console.log("Error fetching data", err);
+      } finally {
+        setLoading(false)
+      }
+    }
+    
+    fetchJobs()
+  }, [])
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -12,9 +29,17 @@ function JobListings({ isHome = false}) {
           {isHome ? 'Browse Jobs': 'Recent Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobListings.map((job) => (
+          {
+            loading ? (
+              <h2>Loading...</h2>
+            ) : (
+              <>
+              {jobs.map((job) => (
                 <JobListing key={job.id} job={job} />
-            ))}
+              ))}
+              </>
+            )
+          }
         </div>
       </div>
     </section>
