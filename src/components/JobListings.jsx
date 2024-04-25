da4a56b2ef85deaf318fed2cca3d18e2396e37a8
Prince Kanu <kanuprince93@yahoo.com>
2024-04-25 12:33:24 -0500

    added react spinner libary, create spinner componenent and replace loading prompt with it

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react'
 import React from 'react'
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner'
+
 
 function JobListings({ isHome = false}) {
   const [jobs, setJobs] = useState([])
@@ -28,19 +30,17 @@ function JobListings({ isHome = false}) {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? 'Browse Jobs': 'Recent Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {
             loading ? (
-              <h2>Loading...</h2>
+              <Spinner loading={loading} />
             ) : (
-              <>
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-              </>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map((job) => (
+                  <JobListing key={job.id} job={job} />
+                ))}
+              </div>
             )
           }
-        </div>
       </div>
     </section>
   )
