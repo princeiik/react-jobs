1d8b197cbc9a2ccc45f4ad3d4e83100c2d89db25
Prince Kanu <kanuprince93@yahoo.com>
2024-04-28 22:41:34 -0500

    create a data loader for job page and use it for job page route in app.js

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,35 +1,46 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+import { useParams, useLoaderData } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 function JobPage() {
     const { id } = useParams()
-    const [job, setJob] = useState(null)
-    const [loading, setLoading] = useState(true)
-
+    const job = useLoaderData()
     
-
-
-    // Using useEffect to fetch data to the Job Page 
-
-    // useEffect( () => {
-    //    const fetchJob = async () => {
-    //     try {
-    //         const res = await fetch(`/api/jobs/${id}`)
-    //         const data = await res.json()
-    //         setJob(data)
-    //       } catch (err) {
-    //         console.log("Error fetching data", err);
-    //       } finally {
-    //         setLoading(false)
-    //       }
-    //    }
-       
-    //    fetchJob()
-    // })
-  
-    return loading ? <Spinner /> : (<h1> {job.title} </h1>)
+    return <h1>{job.title}</h1>
 }
 
-export default JobPage
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`)
+    const data = await res.json()
+    return data
+}
+
+export { JobPage as default, jobLoader }
+
+
+
+//States for 
+
+// const [job, setJob] = useState(null)
+// const [loading, setLoading] = useState(true)
+
+// Using useEffect to fetch data to the Job Page 
+
+// useEffect( () => {
+//    const fetchJob = async () => {
+//     try {
+//         const res = await fetch(`/api/jobs/${id}`)
+//         const data = await res.json()
+//         setJob(data)
+//       } catch (err) {
+//         console.log("Error fetching data", err);
+//       } finally {
+//         setLoading(false)
+//       }
+//    }
+   
+//    fetchJob()
+// })
+
+// return loading ? <Spinner /> : (<h1> {job.title} </h1>)
