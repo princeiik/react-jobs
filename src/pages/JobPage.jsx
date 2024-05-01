c85f04e63c3fdba50f273dbe2338722e9a80276a
Prince Kanu <kanuprince93@yahoo.com>
2024-04-30 22:51:42 -0500

    finishing adding dynamic code to job entry

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -36,7 +36,7 @@ function JobPage() {
                         className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
                       >
                         <FaMapMarker className='text-orange-700 mr-2' />
-                        <p className="text-orange-700">Boston, MA</p>
+                        <p className="text-orange-700">{job.location}</p>
                       </div>
                     </div>
 
@@ -46,12 +46,12 @@ function JobPage() {
                       </h3>
 
                       <p className="mb-4">
-                       We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
+                       {job.description}
                       </p>
 
                       <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-                      <p className="mb-4">$70k - $80K / Year</p>
+                      <p className="mb-4">{job.salary} / Year</p>
                     </div>
                   </main>
 
@@ -60,10 +60,10 @@ function JobPage() {
                     <div className="bg-white p-6 rounded-lg shadow-md">
                       <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                      <h2 className="text-2xl">NewTek Solutions</h2>
+                      <h2 className="text-2xl">{job.company.name}</h2>
 
                       <p className="my-2">
-                        NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
+                        {job.company.description}
                       </p>
 
                       <hr className="my-4" />
@@ -71,21 +71,21 @@ function JobPage() {
                       <h3 className="text-xl">Contact Email:</h3>
 
                       <p className="my-2 bg-indigo-100 p-2 font-bold">
-                        contact@newteksolutions.com
+                        {job.company.contactEmail}
                       </p>
 
                       <h3 className="text-xl">Contact Phone:</h3>
 
-                      <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+                      <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
                     </div>
 
                     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                       <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                      <a
-                        href="/add-job.html"
+                      <Link
+                        to={`/jobs/edit/${job.id}`}
                         className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >Edit Job</a
-                      >
+                        >Edit Job
+                      </Link>
                       <button
                         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                       >
