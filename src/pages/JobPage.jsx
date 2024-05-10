dad0865a639687379cb4bcc360cf17ed9bcb354d
Prince Kanu <kanuprince93@yahoo.com>
2024-05-09 23:52:20 -0500

    add comments to code

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -34,7 +34,8 @@ function JobPage({ deleteJob }) {
                 </Link>
               </div>
             </section>
-
+            
+            {/* Job information */}
             <section className="bg-indigo-50">
               <div className="container m-auto py-10 px-6">
                 <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
