cadd0f9215405213a3d4481222f1655f6a3c79be
Prince Kanu <kanuprince93@yahoo.com>
2024-05-14 00:39:09 -0500

    add comment to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
             <div
               className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
             >
-              {/* <!-- Logo --> */}
+              {/* React Logo */}
               <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
                 <img
                   className="h-10 w-auto"
